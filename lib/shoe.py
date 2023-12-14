4975d9bb0c066659b404b743394b6f6e4b160779
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-12-14 13:22:44 -0500

    Uploading changes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = 0
+        self.size = size
+        self.condition = "a"
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+        
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self._size = size
+
+    # @property
+    # def condition(self):
+    #     return self._condition
+    
+
+    def cobble(self):
+        self.condition = 'New'
+        print('Your shoe is as good as new!')
+
